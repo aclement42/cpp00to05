e27010e3d744bca8dcc43a06200a9a7e91402ac2
Ambre CLEMENTE DI SAN LUCA <aclement@bess-f2r1s15.clusters.42paris.fr>
2024-01-17 19:22:14 +0100

    ex00

---
+++ cpp/cpp06/ex00/main.cpp
@@ -0,0 +1,12 @@
+#include "ScalarConver.hpp"
+
+int main(int ac, char **av)
+{
+    if (ac == 1)
+        std::cout << "Error: Parameter is missing!" << std::endl;
+    else if (ac > 2)
+        std::cout << "Error: One parameter is required!" << std::endl;
+    else
+        ScarlarConverter::convert(av[1]);
+    return(0);
+}
